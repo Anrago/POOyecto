95fbc0efc18266019b5b9f4845f6a97eae621834
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-05-17 20:48:48 -0700

    ola

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*      POOYECTO-SNAKE AND LADDERS
+
+El pooyecto sera un serpientes y escaleras
+
+
+COMANDOS
+cd buidl
+mingw32-make
+./name.exe
+
+*/
+
 
 int main()
 {
-    InitWindow(500, 500, "SerpientesyEscaleras");
+    //Ventana
+    int screenWidth = 1280;
+    int screenHeight = 720;
+    InitWindow(screenWidth, screenHeight,"SerpientesyEscaleras");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Hola mundo",screenWidth/2,screenHeight/2,24,BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
