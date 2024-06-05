198d8bc39c3d6af5917c8bf586ff8e84e47b0b69
Anrago <141792891+Anrago@users.noreply.github.com>
2024-06-04 21:42:30 -0700

    a

--- src/Board.h
+++ src/Board.h
@@ -1,13 +1,20 @@
 #pragma once
+#include <vector>
 #include "Player.h"
 #include "Dice.h"
 #include "Snake.h"
 #include "Ladder.h"
+
+using std::vector;
 class Board
 {
 private:
-    
+    int Cells;
+    vector<Snake> snakes;
+    vector<Ladder> ladders;
+    Player players[];
+    Dice dices[];
 public:
-    Board(/* args */);
+    Board();
 
 };
