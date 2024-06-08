989363afea2979b6a503fdfbe0b430da6151f41c
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-06-08 13:52:02 -0700

    Menu update

---
+++ src/Menu.h
@@ -0,0 +1,13 @@
+#include "Board.h"
+
+void DrawMenu(int screenWidth, int screenHeight){
+    bool finish = false;
+    
+    while (finish== false)
+    {
+        BeginDrawing();
+            
+        EndDrawing();
+    }
+
+}
